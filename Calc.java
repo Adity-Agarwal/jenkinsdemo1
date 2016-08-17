7d16ff1bb4d377b4d796eabab5780792029e7d67
rahulsengupta <rahulsengupta1988@gmail.com>
2016-08-17 01:34:33 -0400

    initial commit

---
+++ Calc.java
@@ -0,0 +1,86 @@
+package Calculator;
+
+public class Calc {
+
+	public int add(int a , int b){
+
+		return (a+b);
+	}
+
+	public int subtract(int a , int b){
+
+		return (a-b);
+	}
+
+	public int multiply(int a , int b){
+
+		return (a*b);
+	}
+
+	public int divide(int a , int b){
+
+		return (a/b);
+	}
+
+	public int mod(int a , int b){
+
+		return (a%b);
+	}
+	
+	public int bitand(int a , int b){
+
+		return (a&b);
+	}
+	
+	public int bitor(int a , int b){
+
+		return (a|b);
+	}
+	
+	public int shiftleft(int a , int b){
+
+		return (a<<b);
+	}
+	
+		public int shiftright(int a , int b){
+
+		return (a>>b);
+	}
+	
+		public int bitxor(int a , int b){
+
+		return (a^b);
+	}
+		
+		public int increment(int a){
+			a = a++;
+			return (a);
+		}
+		
+		public int decrement(int a){
+			a = a--;
+			return (a);
+		}
+		
+		public int unaryminus(int a){
+
+			a = -a;
+			return (a);
+		}
+		
+		public int unaryplus(int a){
+			a = +a;
+			return (a);
+		}
+		
+		public boolean unarynot(boolean a){
+			a = !a;
+			return (a);
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
